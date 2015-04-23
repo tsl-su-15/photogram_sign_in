e084dd393564de194acdc59430f0abb2e2beb054
Arjun Venkataswamy <avenkat2@gmail.com>
2015-04-23 12:05:07 -0500

    rolls back db

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105190754) do
-
-  create_table "photos", force: true do |t|
-    t.string   "source"
-    t.text     "caption"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
