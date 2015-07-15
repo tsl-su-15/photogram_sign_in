c6b02dc677f98b6686365e406d2c7a6f2a291a18
Arjun Venkataswamy <avenkat2@gmail.com>
2015-07-15 09:17:04 -0500

    completes new and create actions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,16 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params['id'])
   end
+
+  def new_form
+  end
+
+  def create_row
+    @photo = Photo.new
+    @photo.caption = params['the_caption']
+    @photo.source = params['the_source']
+    @photo.save
+
+    redirect_to "/photos/#{@photo.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # Routes to CREATE photos
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 end
