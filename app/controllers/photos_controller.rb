bddc1b648028e44e6b6ef7099cf18ae46fba574a
Arjun Venkataswamy <avenkat2@gmail.com>
2015-08-03 11:45:01 -0500

    adds form_for, partials and mass assignment to photo model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
   before_action :check_if_owner, only: [:destroy, :edit, :update]
 
   def check_if_owner
@@ -9,7 +10,10 @@ class PhotosController < ApplicationController
   end
 
   def show
-    @photo = Photo.find(params['id'])
+    respond_to do |format|
+      format.html
+      format.json { render json: @photo}
+    end
   end
 
   def new
@@ -17,10 +21,8 @@ class PhotosController < ApplicationController
   end
 
   def create
-    @photo = Photo.new
-    @photo.caption = params['the_caption']
-    @photo.source = params['the_source']
-    @photo.user_id = params['the_user_id']
+    @photo = Photo.new(photo_params)
+    @photo.user_id = current_user.id
 
     if @photo.save
       redirect_to photo_url(@photo.id)
@@ -31,23 +33,18 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    @photo = Photo.find(params['id'])
     @photo.destroy
 
     redirect_to photos_url
   end
 
   def edit
-    @photo = Photo.find(params['id'])
   end
 
   def update
-    @photo = Photo.find(params['id'])
-    @photo.caption = params['the_caption']
-    @photo.source = params['the_source']
-    @photo.user_id = params['the_user_id']
+    @photo.user_id = current_user.id
 
-    if @photo.save
+    if @photo.update_attributes(photo_params)
       redirect_to photo_url(@photo.id)
     else
       render 'edit'
@@ -58,7 +55,22 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
     @comment = Comment.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @photos }
+    end
   end
+
+  private
+
+    def set_photo
+      @photo = Photo.find(params[:id])
+    end
+
+    def photo_params
+      params.require(:photo).permit(:caption, :source)
+    end
 end
 
 
